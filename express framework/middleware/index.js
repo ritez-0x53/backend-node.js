c960cec273cd6ff528aa1258585749d18ec55a1e
ritez-0x53 <riteswarsrgy@gmail.com>
2025-06-16 22:51:44 +0530

    added connect_db_mysql file

---
+++ express framework/middleware/index.js
@@ -0,0 +1,10 @@
+
+
+// Types of Middleware:
+
+// Application-level middleware: Bound to an instance of the app using app.use() or app.METHOD().
+// Router-level middleware: Bound to an instance of express.Router().
+// Error-handling middleware: Defined with four parameters, used to handle errors.
+// Built-in middleware: Provided by Express, such as express.json() and express.urlencoded().
+// Third-party middleware: Middleware created by the community, such as morgan for logging and body-parser for parsing request bodies.
+
