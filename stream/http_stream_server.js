0b136ff0b7208b4191b3d1706aaba688cee44aa9
ritez-0x53 <riteswarsrgy@gmail.com>
2025-06-22 18:18:50 +0530

    implenting various stream methods in http server

--- stream/http_stream_server.js
+++ stream/http_stream_server.js
@@ -2,21 +2,19 @@
 const http = require("node:http");
 const fs = require("node:fs");
 const path = require("node:path");
+const { pipeline } = require("node:stream");
 
 const server = http.createServer();
 
-server.on("request" , (req,res)=> {
-    console.info("request coming with endpoint "+req.url);
-    if(req.url === "/favicon.ico"){return res.end()};
+server.on("request", async (req, res) => {
+    console.info("request coming with endpoint " + req.url);
+    if (req.url === "/favicon.ico") { return res.end() };
 
-    if(req.url === "/video"){
-        res.writeHead(200 , {
-            "content-type":"video/mp4"
+    if (req.url === "/video") {
+        res.writeHead(200, {
+            "content-type": "video/mp4"
         })
         const stream = fs.createReadStream("data/video.mp4");
-
-        // fs.readFile("data/video.mp4" , (err,data)=> res.end(data) );
-
         // stream.on("data" , (chunk)=>{
         //     res.write(chunk);
         //     stream.resume();
@@ -30,11 +28,26 @@ server.on("request" , (req,res)=> {
         //     console.log("stream paused...")
         // })
 
-        fs.createReadStream("data/video.mp4").pipe(res);
+
+        // fs.readFile("data/video.mp4" , (err,data)=> res.end(data) );
+
+        // fs.createReadStream("data/video.mp4").pipe(res);
+
+        // best way for streaming -- production
+        pipeline(
+            fs.createReadStream(path.join(__dirname, "data/video.mp4")),
+            res,
+            (err) => {
+                if (err) { console.error(err) }
+                else {
+                    console.log("pipeline success")
+                }
+            }
+        )
     }
 
 })
 
-server.listen(8080 , ()=> {
+server.listen(8080, () => {
     console.info("server running on 8080 of lh")
 });
