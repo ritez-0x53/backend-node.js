8e19fe112cba48ad802077d71dbc430c423ba580
ritez-0x53 <riteswarsrgy@gmail.com>
2025-06-23 20:48:36 +0530

    added ejs files with bootstrap

--- SSR-view_engine/app.js
+++ SSR-view_engine/app.js
@@ -4,18 +4,32 @@ const path = require("path");
 const ejs = require("ejs")
 const server = express();
 
-server.set("view engine" , "ejs");
+server.set("view engine", "ejs");
 
-server.get("/test" ,async (req , res)=> {
+server.get("/", async (req, res) => {
+    res.render("home")
+})
+
+server.get("/profile", async (req, res) => {
+    res.render("profile")
+})
+
+server.get("/contact", async (req, res) => {
+
+    res.render("contact")
+})
+
+server.get("/users", async (req, res) => {
 
     const users = [
-        {name : "Riteswar", role : "Full Stack Engineer"},
-        {name : "Sofiya", role : "FOA"},
-        {name : "Simanta", role : "Blockchain Engineer"},
+        { name: "Riteswar", role: "Full Stack Engineer" },
+        {name : "Sofiya" , role : "FOA"},
+        {name : "Chai" , role : "Java Programmer"},
+        {name : "Lisa" , role : "Network Admin"},
+        {name : "John" , role : "AI Engineer"}
     ]
-
-    res.render("home" , {users})
-
+    res.render("users", { users })
 })
+
 
 server.listen(4040);
